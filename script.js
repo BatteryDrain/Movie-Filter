489619b757dafa4d93de62f4a308d2b7bb5c79f5
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-24 16:50:52 -0700

    fixed error

--- script.js
+++ script.js
@@ -146,49 +146,53 @@ submit.addEventListener("click", function(){
 
 filter_select.addEventListener("change", () => {
     FILTER = filter_select.value;
+
+    if(FILTER == "select one"){
+        FILTER = "all";
+    }
     console.log(FILTER);
 
-    for(i = 0; i < 7; i++){
-        if(FIGURES[i].classList == "hide"){
+    for(i = 0; i < FIGURES.length; i++){
+        if(FIGURES[i].classList.contains("hide")){
             FIGURES[i].classList.toggle("hide");
         }
     }
-    if(RATTING == "pg-13"){
+    if(FILTER == "pg-13"){
         for(i = 0; i < 7; i++){
             if(FIGURES[i].dataset.ratted == "pg"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(RATTING == "all"){
+    if(FILTER == "all"){
         for(i = 0; i < 7; i++){
             if(FIGURES[i].classList == "hide"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(AGE == 1900){
+    if(FILTER == 1900){
         for(i = 0; 1 < 7; i++){
             if(FIGURES[i].dataset.agemade < 2000){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(AGE == "all"){
+    if(FILTER == "all"){
         for(i = 0; 1 < 7; i++){
             if(FIGURES[i].classList == "hide"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(ANIMATION == "live_action"){
+    if(FILTER == "live_action"){
         for(i = 0; 1 < 7; i++){
             if(FIGURES[i].dataset.animstion == "t"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(ANIMATION == "animated"){
+    if(FILTER == "animated"){
         for(i = 0; 1 < 7; i++){
             if(FIGURES[i].classList == "hide"){
                 FIGURES[i].classList.toggle("hide");
