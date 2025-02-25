1397ee43fc12067d144354c8863b4cff4b13db86
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-02-24 17:06:39 -0700

    it sort of works!

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ console.log(FIGURES);
 
 filter_select.addEventListener("change", () => {
     FILTER = filter_select.value;
-
-    if(FILTER == "select one"){
-        FILTER = "all";
-    }
     console.log(FILTER);
 
     for(i = 0; i < FIGURES.length; i++){
@@ -17,43 +13,43 @@ filter_select.addEventListener("change", () => {
         }
     }
     if(FILTER == "pg-13"){
-        for(i = 0; i < 7; i++){
+        for(i = 0; i < FIGURES.length; i++){
             if(FIGURES[i].dataset.ratted == "pg"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(FILTER == "all"){
-        for(i = 0; i < 7; i++){
-            if(FIGURES[i].classList == "hide"){
+    if(FILTER == "pg"){
+        for(i = 0; i < FIGURES.length; i++){
+            if(FIGURES[i].dataset.ratted == "pg-13"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
     if(FILTER == 1900){
-        for(i = 0; 1 < 7; i++){
+        for(i = 0; i < FIGURES.length; i++){
+            if(FIGURES[i].dataset.agemade > 1999){
+                FIGURES[i].classList.toggle("hide");
+            }
+        }
+    }
+    if(FILTER == 2000){
+        for(i = 0; i < FIGURES.length; i++){
             if(FIGURES[i].dataset.agemade < 2000){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
-    if(FILTER == "all"){
-        for(i = 0; 1 < 7; i++){
-            if(FIGURES[i].classList == "hide"){
-                FIGURES[i].classList.toggle("hide");
-            }
-        }
-    }
     if(FILTER == "live_action"){
-        for(i = 0; 1 < 7; i++){
+        for(i = 0; i < FIGURES.length; i++){
             if(FIGURES[i].dataset.animstion == "t"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
     }
     if(FILTER == "animated"){
-        for(i = 0; 1 < 7; i++){
-            if(FIGURES[i].classList == "hide"){
+        for(i = 0; i < FIGURES.length; i++){
+            if(FIGURES[i].dataset.animstion == "f"){
                 FIGURES[i].classList.toggle("hide");
             }
         }
